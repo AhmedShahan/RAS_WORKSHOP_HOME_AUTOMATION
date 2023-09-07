6206975e87b8b4c9b59b2c7095a71de5c7f05327
AhmedShahan <shahan.ahmed@northsouth.edu>
2023-09-07 10:27:27 +0600

    new way of rfid_uid

--- RFID_GET_UID.ino
+++ RFID_GET_UID.ino
@@ -1,4 +1,4 @@
-#define SS_PIN 4  //D2
+#define SS_PIN 16  //D0
 #define RST_PIN 5 //D1
 #include <SPI.h>
 #include <MFRC522.h>
@@ -10,21 +10,13 @@ void setup()
   Serial.begin(9600);   // Initiate a serial communication
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
-  Serial.println("Show your card:");
 }
 void loop() 
 {
   // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  if ( mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial())
   {
-    return;
-  }
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) 
-  {
-    return;
-  }
-  String content= "";
+      String content= "";
   for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
@@ -34,4 +26,5 @@ void loop()
   String rfid=content.substring(1);
   Serial.println(rfid);
 
+  }
 }
